d1a6dad7a3db2e0cfda30dbb48a2e42384a3271c
Aaron Greider <40152187+aarongreider@users.noreply.github.com>
2026-09-04 12:47:08 -0400

    rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+      rollupOptions: {
+        output: {
+          dir: './dist/',
+          entryFileNames: 'jj-aaron-winefest-vendor-dashboard-1.0.0.js',
+          assetFileNames: 'jj-aaron-winefest-vendor-dashboard.css',
+        }
+      }
+    },
 })
